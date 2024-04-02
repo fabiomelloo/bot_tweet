def9d3b92241694dfd72e0530a5a32d7e8d57c3d
fabiomelloo <fabiomelocastro@gmail.com>
2024-04-02 08:48:05 -0400

    testando aqui 2

--- home_1.py
+++ home_1.py
@@ -5,7 +5,7 @@ bearer_token = r"AAAAAAAAAAAAAAAAAAAAAMF0swEAAAAAIAO"
 access_token = "887732196211142659-EY0SgnQeYZrFpbP68c7aRDzi1Oq6OOa"
 access_token_secret = "CMJ0PT3upBzpWj6UIxVHLkT8pggHBQful3H421b8PXhWO"
 
-# Autenticar
+# Autenticarss
 cleint = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(consumer_key,consumer_secret,access_token,access_token_secret)
 api = tweepy.API(auth)
